d121ec6fdf33c4b59ba73830f8146c6eb26befff
Robbie Shaw <36793869+robbie6shaw@users.noreply.github.com>
2020-05-29 19:40:43 +0100

    Final Changes

--- navbar.js
+++ navbar.js
@@ -2,21 +2,20 @@ var navigation = {"links":
     [
         {
             "url":"index.html",
-            "name":"Home"
+            "name":"About"
         },
         {
             "url":"https://www.notion.so/rshaw/School-Notes-882d824f9d3b4d30945a25c5b2dbdc1e",
             "name":"Notes"
         },
         {
-            "url":"othersites.html",
-            name:"On Other Sites"
-        },
-        {
             "url":"photo.html",
             "name":"Photos"
+        },
+        {
+            "url":"othersites.html",
+            name:"On Other Sites"
         }
-        
 ]};
 
 var template = Handlebars.compile($("#nav-elements").html());
